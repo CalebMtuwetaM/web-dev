8b35b1b6cd12ab2f520357446db292d844b11a1d
Caleb <73331147+westillest@users.noreply.github.com>
2023-04-08 19:57:01 +0000

    Added dynamic data and API route

--- app.py
+++ app.py
@@ -1,8 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
+Products = [
+  {
+    'name': 'Wrist watch',
+    'Description': 'New',
+    'Price': 'ksh 1500'
+  },
+  {
+    'name': 'Wrist watch',
+    'Description': 'New',
+    'Price': 'ksh 1500'
+  },
+  {
+    'name': 'Wrist watch',
+    'Description': 'New',
+    'Price': 'ksh 1500'
+  },
+  {
+    'name': 'Wrist watch',
+    'Description': 'New',
+    'Price': 'ksh 1500'
+  },
+  {
+    'name': 'Wrist watch',
+    'Description': 'New',
+    'Price': 'ksh 1500'
+  },
+  {
+    'name': 'Wrist watch',
+    'Description': 'New',
+    'Price': 'ksh 1500'
+  },{
+    'name': 'Wrist watch',
+    'Description': 'New',
+    'Price': 'ksh 1500'
+  },{
+    'name': 'Wrist watch',
+    'Description': 'New',
+    'Price': 'ksh 1500'
+  },
+]
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',products=Products)
+
+@app.route("/api/products")
+def list_products():
+  return jsonify(Products)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
