73569c4b43bb9dfbe263dca302b1859522271161
Caleb <73331147+westillest@users.noreply.github.com>
2023-04-12 01:48:19 +0000

    added register and login forms

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify,url_for,flash,redirect
 from database import load_products_from_db,load_product_from_db
-
+from forms import RegistrationForm,LoginForm
 
 app = Flask(__name__)
 
@@ -20,6 +20,32 @@ def show_product(id):
   if not product:
     return "Not Found",404
   return render_template('productpage.html',product=product)
+from flask_wtf.csrf import CSRFProtect
+
+
+app.config['SECRET_KEY'] = 'your_secret_key_here' # Set your secret key here
+csrf = CSRFProtect(app)
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!","success")
+        return redirect(url_for("home"))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check username and password','danger')
+    return render_template("login.html",title="Login",form=form)
+
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
